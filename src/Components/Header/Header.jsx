4eaa80c0c89b89fa6bd6e29fd216ca6de6bffbf4
Joao Vitor Machado <joaovitormdesa@gmail.com>
2022-07-29 13:35:47 -0300

    Bug Fix: Barra de pesquisa não estrapola tamanho max

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,18 +8,21 @@ const Header = ({ qtd, Products }) => {
   let searchresults = [];
 
   const search = (e) => {
-    const results = document.querySelector("#search-results");
+    const results = document.querySelector(".search-results");
 
     if (e.target.value.length > 0) {
       const exp = new RegExp(e.target.value, "i");
       searchresults = Products.filter((item) => exp.test(item.Name));
       results.innerHTML = "";
+      results.classList.add("height")
       searchresults.map(({id, category, image,Name,Price,Desc}) => {
         results.innerHTML += `<a href="${Name}"> <img src="${image}"/> ${Name}</a>`;
       })
     } else {
       searchresults = [];
       results.innerHTML = "";
+            results.classList.remove("height");
+
     }
   };
 
@@ -29,13 +32,7 @@ const Header = ({ qtd, Products }) => {
         <Link to="/" className="header-Logo">
           <img src={Logo} alt="AluraGeek Logo" />
         </Link>
-        <div className="search-container">
           <input type="text" name="Search" id="search" onChange={search} />
-
-          <div
-            id="search-results"
-          ></div>
-        </div>
         <div className="header-user-field">
           <Link to="/login" className="a">
             <AiOutlineUser className="header-icons user" />
@@ -46,6 +43,7 @@ const Header = ({ qtd, Products }) => {
             <AiOutlineShoppingCart className="header-icons" />
           </Link>
         </div>
+        <div className="search-results"></div>
       </div>
     </>
   );
